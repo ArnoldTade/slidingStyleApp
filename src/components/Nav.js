ef67f355ae3f37ef256138941d5561e9d0fa686b
ArnoldTade <saldivararnold80@gmail.com>
2024-01-05 19:09:42 +0800

    css update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,11 +7,11 @@ function Nav() {
    
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Brand href="#home">SaldiVar</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
+            Signed in as: <a href="#login">Arnold Saldivar</a>
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
