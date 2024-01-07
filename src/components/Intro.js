aee935108cc03daa5e285aec329e7f5aed0199b5
ArnoldTade <saldivararnold80@gmail.com>
2024-01-07 09:55:36 +0800

    image update

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -10,13 +10,15 @@ function Intro() {
       <Row>
         <Col id='img-col'>
              <Image className='img-about' id="img" src={process.env.PUBLIC_URL + '/img/dog.jpg'} alt="Example" rounded />
+             <h1 class="about">About</h1>
         </Col>
         <Col id='img-col'>
             <Image className='img-project' id="img" src={process.env.PUBLIC_URL + '/img/dog.jpg'} alt="Example" rounded />
+            <h1 class="project">Project</h1>
         </Col>
         <Col id='img-col'>
             <Image className='img-contact' id="img" src={process.env.PUBLIC_URL + '/img/dog.jpg'} alt="Example" rounded />
-
+            <h1 class="contact">Contact</h1>
         </Col>
       </Row>
     </Container>
